b4bcdcb93612984cd01259ccddbe35659b8c728f
AdamSiddique <AdamTSiddique@gmail.com>
2023-06-09 12:59:33 +0200

    Fixed buying art

--- ArtBay/ArtBay/queries.py
+++ ArtBay/ArtBay/queries.py
@@ -162,17 +162,13 @@ def get_user_by_user_name(user_name):
 
 def get_all_art():
     sql = """
-    SELECT art_pk as pk, title, medium, price, descrip, artist_name, available, picture, artist_pk
+    SELECT art_pk as art_pk, title, medium, price, descrip, artist_name, available, picture, artist_pk
     FROM vw_art
     """
     order = " ORDER BY price "
     db_cursor.execute(sql + order)
     art = [Art(res) for res in db_cursor.fetchall()] if db_cursor.rowcount > 0 else []
     # Convert to base64 to show on website
-    print(art)
-    for item in art:
-        item['picture'] = print(f"{item['picture']}")
-    print(art)
     return art
 
 
